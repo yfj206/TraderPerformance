1ac2cd25ff752b08b1b2b6f64448858d4653fde0
yamlak <yfj206@nyu.edu>
2024-05-02 14:23:37 -0700

    Init

---
+++ TraderPerformance/Repositories/ITradeRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TraderPerformance.Data;
+using TraderPerformance.Models;
+
+namespace TraderPerformance.Repositories;
+
+public interface ITradeRepository
+{
+    IEnumerable<Trade> GetTradesForPortfolio(Guid portfolioId);
+    string GetPortfolioName(Guid portfolioId);
+}
+
+public class TradeRepository : ITradeRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public TradeRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Trade> GetTradesForPortfolio(Guid portfolioId)
+    {
+        return _context.Trades.Where(t => t.Id == portfolioId).ToList();
+    }
+
+    public string GetPortfolioName(Guid portfolioId)
+    {
+        return _context.Portfolios.FirstOrDefault(p => p.Id == portfolioId)?.Name;
+    }
+}
